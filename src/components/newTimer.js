f2544c065cd0d235f5e4d0f4e53acadb8e7a97b1
TulkasAstaldo <p.ioan9@gmail.com>
2020-06-09 15:21:36 +0300

    added new timer input

---
+++ src/components/newTimer.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { addTimer, NEW_TIMER } from "../actions";
+
+class NewTimer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "" };
+  }
+
+  handleChange = (e) => this.setState({ name: e.target.value });
+  handleClick = (e) => this.props.addTimer(this.state.name);
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="New Timer Name"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleClick}>Save</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {};
+
+const mapDispatchToProps = () => addTimer;
+
+export default connect(mapStateToProps, mapDispatchToProps())(NewTimer);
