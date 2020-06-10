b3c5d09301b2e98a2f7055139832e46687add1ea
TulkasAstaldo <p.ioan9@gmail.com>
2020-06-10 21:35:36 +0300

    persisting timers

--- src/store.js
+++ src/store.js
@@ -1,7 +1,17 @@
 import { createStore } from "redux";
 import reducers from "./reducers";
+import { loadState, saveState } from "./utils";
+import throttle from "lodash/throttle";
+
+const persistedState = loadState();
 
 export const store = createStore(
   reducers,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
 );
